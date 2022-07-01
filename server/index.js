8a841b9a0562c13a2aeefb499bcfe6c0804f021d
Naveen <navaneethk002@gmail.com>
2022-07-01 09:21:37 -0500

    added new files

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", postRoutes);
+
+//connect to mongodb cloud
+const CONNECTION_URL =
+  "mongodb+srv://social_project:ovlZ1M0SITGpBnlv@cluster0.un0hjta.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8800;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
